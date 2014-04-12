f4a36f71e9aa481c9ecf80bd2507e76dd833fe8e
Ravi Netravali <ravinet@mit.edu>
2014-04-12 12:18:13 -0400

    new replay script for all link speed/delay combinations

--- run_replay_delay.py
+++ run_replay_delay.py
@@ -1,14 +1,21 @@
 import os
 import sys
-#dirs =  os.listdir(".")
-#dirs.remove( "record_info.txt" )
-#print dirs
+from time import sleep
+
+#link_speeds = [0.2, 0.34199519, 0.58480355, 1, 1.70997595, 2.92401774, 5, 8.54987973, 14.62008869, 25]
+#delays = [rtt / 2 for rtt in [30, 60, 90, 120, 150, 180, 210, 240, 270, 300]]
+
+link_speeds = [5, 25]
+delays = [rtt / 2 for rtt in [30, 90]]
 
 site_list = sys.argv[1]
-delay = sys.argv[2]
-with open(site_list) as f:
-    for line in f:
-        site = line.split( " " )[0]
-        folder = line.split(" ")[1].strip("\n")
-        os.system( "replayshell /home/ravi/sites/" + folder + " /usr/local/bin/delayshell " + str(delay) + " /usr/local/bin/cellshell 12Mbps_trace.txt 12Mbps_trace.txt /usr/bin/python /home/ravi/run_tests/load_page.py " + site + " " +  str(delay) + " replay")
-        os.system( "sudo rm /tmp/replayshell.*")
+
+for link_speed in link_speeds:
+    for delay in delays:
+        print "link_speed =", link_speed, ", delay =", delay
+
+        with open(site_list) as f:
+            for line in f:
+                site = line.split( " " )[0]
+                folder = line.split(" ")[1].strip("\n")
+                os.system( "replayshell /home/ravi/sites/" + folder + " /usr/local/bin/delayshell " + str(delay) + " /usr/local/bin/linkshell /home/ravi/sites/" + str(link_speed) + "Mbps_trace.txt /home/ravi/sites/" + str(link_speed) + "Mbps_trace.txt /usr/bin/python /home/ravi/sites/load_page.py " + site + " " + str(delay) + " " + str(link_speed) + " replay")
