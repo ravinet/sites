be61ad113e54e33c55de2e59a146fd2360d44cb7
Ravi Netravali <ravinet@mit.edu>
2014-05-09 10:52:54 -0400

    few cleanups

--- site_sizes.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-site_list = sys.argv[1]
-
-with open(site_list) as f:
-    for line in f:
-        folder = line.split(" ")[1].strip("\n")
-        os.system( "du -hbs " + folder + " >> page_sizes.txt" )
